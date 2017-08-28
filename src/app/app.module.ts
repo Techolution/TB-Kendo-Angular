4d45ec8a2acefc6d7bb43f47ac854b1671c20e98
originalusrnm <jshornbuckle@gmail.com>
2017-08-27 22:40:42 -0400

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// Import the Animations module
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Import the ButtonsModule
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { ChartsModule } from '@progress/kendo-angular-charts';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
+import { DialogModule } from '@progress/kendo-angular-dialog';
+
+import 'hammerjs';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    ButtonsModule,
+    ChartsModule,
+    DateInputsModule,
+    DialogModule,
+    GridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
