0703373024b2d7e9638c9e2ebfa704af23b9e3a0
originalusrnm <jshornbuckle@gmail.com>
2017-08-27 22:47:28 -0400

    Update button lab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,22 @@
 import { Component } from '@angular/core';
-import { products } from './products';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
 
-  value: any;
+  disabled: boolean = true;
 
-  public data: any[] = [
-    936, 968, 1025, 999, 998, 1014, 1017, 1010, 1010, 1007
-  ];
-
-  settingData: any = [ { label: 'username' }, { label: 'theme' }];
-
-  sort: any;
-
-  private gridData: any[] = products;
+  disabledToggleOpts: any = [ { label: 'disable' }, { label: 'enable' }];
 
   onButtonClick() {
-      this.title = 'Hello from Kendo UI!';
+      alert('Hello!');
   }
 
   onItemClicked(item) {
-    console.log(item.label);
-  }
-
-  sortChange(event) {
-    this.sort = event;
-    console.log(event);
-  }
-
-  handleSelectionChange(selection) {
-    console.log(selection);
+    this.disabled = item.label === 'enable' ? false : true;
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Import the ButtonsModule
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
-import { ChartsModule } from '@progress/kendo-angular-charts';
-import { GridModule } from '@progress/kendo-angular-grid';
-import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
-import { DialogModule } from '@progress/kendo-angular-dialog';
 
 import 'hammerjs';
 
@@ -22,11 +18,7 @@ import 'hammerjs';
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
-    ButtonsModule,
-    ChartsModule,
-    DateInputsModule,
-    DialogModule,
-    GridModule
+    ButtonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
